6a2f797546209d657d9249e68fa9993be78f89d1
jihad <zoyanopicial@gmail.com>
2025-11-06 12:44:48 +0600

    hero ui install

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,19 +1,41 @@
-import React, { Suspense } from 'react'
-import Navbar from './Navbar'
+import React, { Suspense, useState, useEffect } from "react";
+import Navbar from "./Navbar";
+import { CircularProgress } from "@heroui/react";
 
-const navItemsPromise = fetch("../navitems.json").then(res => res.json()).catch(eror => {
-    console.log(eror)
-})
-
+const navItemsPromise = fetch("../navitems.json")
+  .then((res) => res.json())
+  .catch((error) => {
+    console.error(error);
+  });
 
 function LeftNav() {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((v) => (v >= 100 ? 0 : v + 10));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-        <Suspense fallback={<span>Loading...</span>}>
-            <Navbar navItemsPromise={navItemsPromise}></Navbar>
-        </Suspense>
+      <Suspense
+        fallback={
+          <CircularProgress
+            aria-label="Loading..."
+            color="warning"
+            showValueLabel={true}
+            size="lg"
+            value={value}
+          />
+        }
+      >
+        <Navbar navItemsPromise={navItemsPromise} />
+      </Suspense>
     </div>
-  )
+  );
 }
 
-export default LeftNav
+export default LeftNav;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from "react-router/dom";
 import router from './Router/Router.jsx';
+import { HeroUIProvider } from "@heroui/react";
 
 
 
-createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <HeroUIProvider>
       <RouterProvider router={router}></RouterProvider>
-  </StrictMode>,
-)
+    </HeroUIProvider>
+  </StrictMode>
+);
